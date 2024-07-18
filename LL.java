cf8089711aa0727d4b9896b5ee1d392eaac48eb9
Anton Prince Oommen <96407588+antonnprince@users.noreply.github.com>
2024-07-18 11:58:25 +0530

    added insert at index

--- LL.java
+++ LL.java
@@ -43,23 +43,47 @@ class LinkedList{
         node.next = head;
         head=node;
     }  
+    
+    public void insertNode(int index, int d)
+    {
+        Node node = new Node();
+        node.data = d;
+        node.next=null;
+        Node n = head;
+        
+        if(index==0)
+        {
+           node.next=head;
+           head = node;
+           return;
+        }
+        
+        
+        for(int i=0;i<index-1;i++)
+        {
+            if(n.next==null)
+            return;
+            
+            
+            n=n.next;
+        }
+        
+        node.next = n.next;
+        n.next=node;        
+    }
+    
     public void deleteLL(int index)
     {
         Node n = head;
         Node n1;
-        for(int i=0;i<index-1;i++)
+        
+        for(int i=0;i<index;i++)
         {
             n=n.next;
         }
-        
-        n1 = n.next;
-        
-        n.next = n1.next;
-        
+        n1=n.next;
+        n.next=n1.next;
         n1=null;
-        
-        System.out.println("After deletion ");
-        displayList();
     }
     public void displayList()
     {
@@ -80,7 +104,10 @@ public class LL
 	    l.insertFirst(1);
 	    l.insertFirst(0);
 	    l.insertLast(2);
-	    l.displayList();
-	    l.deleteLL(2);
+	    
+	   // l.deleteLL(2);
+	   l.insertNode(2,1000);
+	   System.out.println("New after insertion is ");
+	   l.displayList();
 	}
 }
