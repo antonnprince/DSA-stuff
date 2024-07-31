a64d2f1243a5bd292f031e14a330887f626fa65e
Anton Prince Oommen <96407588+antonnprince@users.noreply.github.com>
2024-07-31 21:04:27 +0530

    Update LinkedList.java

--- LinkedList.java
+++ LinkedList.java
@@ -10,12 +10,23 @@ public class Main{
         list.add(2);
         list.addFirst(0);
         list.addFirst(-1);
+        list.addLast(100);
         
         System.out.println("0th element is "+list.get(0));
         
         for(Integer element: list )
         {
             System.out.println(element);
+        }
+        
+        list.remove(1);
+        list.removeLast();
+        list.removeFirst();
+        
+        System.out.println("After removal");
+        for(Integer item:list)
+        {
+            System.out.println(item);
         }
     }
 }
