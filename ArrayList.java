2ce6246411383176fe2ad20d1ed30b85c53677d9
Anton Prince Oommen <96407588+antonnprince@users.noreply.github.com>
2024-08-31 19:31:32 +0530

    Create ArrayList.java

---
+++ ArrayList.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+
+public class Main
+{
+	public static void main(String[] args) {
+		List<Integer> arraylist = new ArrayList<>();
+		
+		arraylist.add(0);
+		arraylist.add(1);
+		arraylist.add(2);
+
+    System.out.println(arraylist); //orints element all together as array
+    
+	
+	    for(Integer element: arraylist)
+	    {
+	        System.out.println(element);
+	    }
+		    
+	    
+	}
+}
