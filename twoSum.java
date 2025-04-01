7c0e5d59b5418cfc09f42a29829db95323b93944
Anton Prince Oommen <96407588+antonnprince@users.noreply.github.com>
2025-04-01 17:59:41 +0530

    Update twoSum.java

--- twoSum.java
+++ twoSum.java
@@ -2,7 +2,8 @@ class Solution {
     public int[] twoSum(int[] nums, int target) {
     
     HashMap<Integer, Integer> map = new HashMap<>();
-
+    int sample=0;
+        int test = 0;
     for(int i = 0; i < nums.length;i++)
     {
         int n = nums[i];
